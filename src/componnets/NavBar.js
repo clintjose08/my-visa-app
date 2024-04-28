f749334df02918aef62f40ad565b76e85b4a1e68
Clint Jose <clintjose08@gmail.com>
2024-04-28 18:13:28 +0100

    fix: update favicon

--- src/componnets/NavBar.js
+++ src/componnets/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import { Box } from "@mui/system";
 
 const NavBar = () => {
   return (
@@ -9,7 +10,9 @@ const NavBar = () => {
       style={{ background: "transparent", boxShadow: "none" }}
     >
       <Toolbar>
-        <img src="/logo.png" height="100px" />
+        <Box sx={{ p: 2 }}>
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} height="70px" />
+        </Box>
       </Toolbar>
     </AppBar>
   );
